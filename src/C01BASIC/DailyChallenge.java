f8c77340974f87052d0083db7a3d42b6ff88e1d3
Yoon Sejin <pentum99@gmail.com>
2025-06-17 22:37:59 +0900

    복습완료

---
+++ src/C01BASIC/DailyChallenge.java
@@ -0,0 +1,30 @@
+package C01BASIC;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DailyChallenge {
+    public static void main(String[] args) throws IOException{
+
+////        백준 - 검증수 2475
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String[] input = br.readLine().split(" "); ///String 배열로 입력을 받음
+//        int total = 0; ///출력할 결과값을 의미
+//
+//        for (int i = 0; i < 5; i++) { ///0번부터 4번째 까지의 배열의 값의 곱을 넣는 반복문
+//            int num = Integer.parseInt(input[i]); ///배열에 i번째에 있는 값을 int변수로 지정하여 빼냄
+//            total += num * num; ///그 값의 제곱
+//        }
+//
+//        System.out.println(total % 10); ///문제에서 원하는 값이 10으로 나누고 남은 몫
+
+////        백준 - 구구단
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int input = Integer.parseInt(br.readLine());
+//        for (int i = 0; i <= 9; i++) {
+//            System.out.println(input + " * " + i + " = " + input*i);
+//        }
+
+    }
+}
--- src/C01BASIC/Main.java
+++ src/C01BASIC/Main.java
@@ -3,15 +3,32 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = new int[br.read()];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int total =0;
-        for (int i = 0; i < input.length; i++) {
-            total+= input[i];
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
         }
-        System.out.println(input.length);
-        System.out.println(total);
 
+        List<Integer> result = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.offer(queue.poll()); // 앞에서 꺼내 뒤로
+            }
+            result.add(queue.poll()); // K번째 제거
+        }
+
+        // 출력 형식 맞추기
+        System.out.print("<");
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+            if (i != result.size() - 1) System.out.print(", ");
+        }
+        System.out.println(">");
     }
 }
