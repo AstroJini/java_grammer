95d2dfd0d302dd57da76da2b09ae0a86933cbfce
Yoon Sejin <pentum99@gmail.com>
2025-06-26 00:23:41 +0900

    fix

--- src/C01BASIC/Main.java
+++ src/C01BASIC/Main.java
@@ -6,20 +6,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for(int i=0; i<T; i++){
-            String line = br.readLine();
-            int total = 0;
-            int x =0;
-            for (int j = 0; j < line.length(); j++) {
-                if (line.charAt(j) == 'O'){
-                    x++;
-                    total+=x;
-                }else {
-                    x=0;
-                }
-            }
-            System.out.println(total);
+        String input = br.readLine();
+
+        for(char c='a'; c<='z'; c++){
+            System.out.println(input.indexOf(c)+" ");
         }
+
     }
 }
